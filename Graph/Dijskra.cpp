03bd7f1d2c26cc6ae4f708c2227b5ac39756a848
harshsri28 <66912920+harshsri28@users.noreply.github.com>
2021-10-07 14:59:41 +0530

    Add optimal way

--- Graph/Dijskra.cpp
+++ Graph/Dijskra.cpp
@@ -1,47 +1,120 @@
-#include<bits/stdc++.h>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
+#define all(v) (v).begin(),(v).end()
+#define tc(t) ll t; cin >> t; while (t--)
+#define ll long long
+#define int long long
+#define pb push_back
+#define debug(x) cout<<x<<"\n";
+#define Harsh_Srivastava ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define prec(n) fixed<<setprecision(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
+#define ini(a, i) memset(a, i, sizeof(a))
+#define pii pair<int,int>
+#define psi pair<string,int>
+#define f first
+#define s second
+#define ll long long
+#define vi vector<int>
+#define vii vector<pii>
+#define rep(i,a,b) for (int i=a; i<b; ++i)
+#define rev(i,a,b) for (int i=a; i>b; --i)
+typedef long double lld;
 
-int main(){
-	int n,m,source;
-	cin >> n >> m;
-	vector<pair<int,int> > g[n+1]; 	// 1-indexed adjacency list for of graph
+ll gcd(int a, int b) { if (a == 0) return b; return gcd(b % a, a);}
+ll LCM(ll a, ll b) { return (a / gcd(a, b)) * b; }
+int max(int a, int b) {if (a > b) return a; else return b;}
+int min(int a, int b) {if (a < b) return a; else return b;}
+void pr(int x) {cout << x;}
+void prl(int x) {cout << x << "\n";}
+// Function to find the nCr
+void printNcR(int n, int r)
+{   long long p = 1, k = 1;
+    if (n - r < r) r = n - r;
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+            long long m = __gcd(p, k);
+            p /= m;
+            k /= m;
+            n--;
+            r--;
+        }
+    }
+    else
+        p = 1;
+    cout << p << endl;
+}
+// expo
+lld expo1(lld a, ll b)
+{
+    lld res = 1.0;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
-	int a,b,wt;
-	for(int i = 0; i<m ; i++){
-		cin >> a >> b >> wt;
-		g[a].push_back(make_pair(b,wt));
-		g[b].push_back(make_pair(a,wt));
-	}	
-	
-	cin >> source;
-	
-	// Dijkstra's algorithm begins from here
-	priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > pq;// min-heap ; In pair => (dist,from)
-	vector<int> distTo(n+1,INT_MAX); 	// 1-indexed array for calculating shortest paths; 
-	
-	distTo[source] = 0;
-	pq.push(make_pair(0,source));	// (dist,from)
-	
-	while( !pq.empty() ){
-		int dist = pq.top().first;
-		int prev = pq.top().second;
-		pq.pop();
-		
-		vector<pair<int,int> >::iterator it;
-		for( it = g[prev].begin() ; it != g[prev].end() ; it++){
-			int next = it->first;
-			int nextDist = it->second;
-			if( distTo[next] > distTo[prev] + nextDist){
-				distTo[next] = distTo[prev] + nextDist;
-				pq.push(make_pair(distTo[next], next));
-			}
-		}
-		
-	}
-	
-	cout << "The distances from source, " << source << ", are : \n";
-	for(int i = 1 ; i<=n ; i++)	cout << distTo[i] << " ";
-	cout << "\n";
-	
-	return 0;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 1;
+const int len = 26;
+const ll INF = LLONG_MAX;
+
+int vis[100005], dis[100005];
+vii adj[100005];
+
+void solve() {
+    int n, m; cin >> n >> m;
+    rep(i, 0, m) {
+        int u, v, w;
+        cin >> u >> v >> w;;
+        adj[u].pb({v, w});
+    }
+    //dijkstra
+    rep(i, 2, n + 1) dis[i] = INF;
+
+    priority_queue<pii, vii, greater<pii>> q;
+    q.push({0, 1});
+
+    while (!q.empty()) {
+        int u = q.top().s; q.pop();
+        if (vis[u]) continue;
+        vis[u] = 1;
+        for (auto [v, w] : adj[u]) {
+            if (dis[v] > dis[u] + w) {
+                dis[v] = dis[u] + w;
+                q.push({dis[v], v});
+            }
+        }
+    }
+    rep(i, 1, n + 1) cout << dis[i] << " ";
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+
+    Harsh_Srivastava;
+    // ll t; cin >> t;
+    // for (int i = 1; i <= t; ++i) {
+    //     solve();
+    //     cout << "Case #" << i << ":" << " " << x << "\n";
+    // }
+    // tc(t)
+    solve();
 }
